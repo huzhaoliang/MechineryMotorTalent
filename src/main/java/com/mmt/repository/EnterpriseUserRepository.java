5e81f16ed0c21078be7a981628d7d7e845ae17a6
hp04301986 <hp393747035@126.com>
2019-02-25 18:11:38 +0800

    add one repository

---
+++ src/main/java/com/mmt/repository/EnterpriseUserRepository.java
@@ -0,0 +1,8 @@
+package com.mmt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mmt.entity.EnterpriseUser;
+
+public class EnterpriseUserRepository extends JpaRepository<EnterpriseUser, Long>{
+
+}
