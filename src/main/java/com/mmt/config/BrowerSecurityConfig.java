1fcb2f5b4e17396907aa5a1e38606991b41ef3f9
hp04301986 <hp393747035@126.com>
2019-03-01 12:28:26 +0800

    add css and js

--- src/main/java/com/mmt/config/BrowerSecurityConfig.java
+++ src/main/java/com/mmt/config/BrowerSecurityConfig.java
@@ -12,7 +12,7 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter{
         // http.authorizeRequests()每个匹配器按照它们被声明的顺序被考虑。
         http.authorizeRequests()
             // 所有用户均可访问的资源
-            .antMatchers("/verify","/main","/static/css/**","/static/js/**","/static/images/**","/static/bootstrap/css/**","/static/bootstrap/js/**").permitAll()
+            .antMatchers("/verify","/main","/static/font-awesome-4.7.0/**","/static/fonts/**","/static/icons-reference/**","/static/css/**","/static/js/**","/static/img/**","/static/bootstrap/**").permitAll()
             // ROLE_USER的权限才能访问的资源
             //.antMatchers("/manage/**").hasRole("MANAGE")
             // 任何尚未匹配的URL只需要验证用户即可访问
