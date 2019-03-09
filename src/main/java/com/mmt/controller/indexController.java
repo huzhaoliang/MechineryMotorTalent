4ebafc168110ca55263bfb3e741e27e46772ef1e
HuZhaoLiang <HuZhaoLiang@192.168.31.55>
2019-03-09 23:09:42 +0800

    add a file

---
+++ src/main/java/com/mmt/controller/indexController.java
@@ -0,0 +1,11 @@
+package com.mmt.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class indexController {
+
+}
