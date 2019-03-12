b170c50a3439a3dc0a6b97ecbf01f9ce09a5f08a
HuZhaoLiang <HuZhaoLiang@10.91.4.182>
2019-03-12 20:15:55 +0800

    add index class

--- src/main/java/com/mmt/controller/indexController.java
+++ src/main/java/com/mmt/controller/indexController.java
@@ -6,7 +6,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
 public class indexController 
 {
-
+	@RequestMapping(value = "/")
+	public String index() {
+		System.out.println("++++++to index.html+++++");
+		return "index";
+	}
 }
