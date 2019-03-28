3911f3101b68b8761d3304142ef69d4c6034a41c
hp04301986 <hp393747035@126.com>
2019-03-28 15:17:38 +0800

    update enterpriseRelated

--- src/main/java/com/mmt/service/EnterpriseService.java
+++ src/main/java/com/mmt/service/EnterpriseService.java
@@ -1,5 +1,7 @@
 package com.mmt.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 
 import com.mmt.entity.EnterpriseUser;
@@ -8,4 +10,6 @@ public interface EnterpriseService {
 	EnterpriseUser insertEnterprise(EnterpriseUser user);
 	
 	Page<EnterpriseUser> getEnterpriseUsers(int pageNumber, int pageSize);
+	
+	List<EnterpriseUser> getAllEnterprise();
 }
--- src/main/java/com/mmt/service/impl/EnterpriseServiceImpl.java
+++ src/main/java/com/mmt/service/impl/EnterpriseServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mmt.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -25,6 +27,11 @@ public class EnterpriseServiceImpl implements EnterpriseService{
 		PageRequest request = PageRequest.of(pageNumber - 1, pageSize, null);
 		Page<EnterpriseUser> enterprises = enterpriseRepository.findAll(request);
 		return enterprises;
+	}
+
+	@Override
+	public List<EnterpriseUser> getAllEnterprise() {
+		return enterpriseRepository.findAll();
 	}
 
 }
