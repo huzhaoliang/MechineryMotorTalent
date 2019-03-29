10fb3474235007530bdef1991e4774ac0e4a61a4
HuZhaoLiang <HuZhaoLiang@zhaoliangs-mbp>
2019-03-29 21:16:05 +0800

    add signup controller

---
+++ src/main/java/com/mmt/controller/SignUpController.java
@@ -0,0 +1,19 @@
+package com.mmt.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SignUpController 
+{
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@RequestMapping(value="/signup")
+	public String signup() {
+		logger.info("++++++++Sign up++++++++++");
+		return "signup";
+	}
+}
