be73b85ef467d30f05863ac1486a89cd4b24067c
huz7 <huz7@fe80:0:0:0:2de:c673:a6b9:cbb3%12>
2019-04-02 20:33:51 +0800

    add index relevant

--- src/main/resources/static/mmt/index.js
+++ src/main/resources/static/mmt/index.js
@@ -1,15 +1,56 @@
 "use strict";
+//varible
+let email = null;
+let emailFormat = /^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g;
+
+//const
+
+
+
 //function
 function verifyEmail()
 {
 	
+	if($("#textEmail").val()==undefined||$("#textEmail").val()==null||$("#textEmail").val()=="")
+	{
+		alert("邮箱不能为空");
+		return
+	}
+	else
+	{
+		email = $("#textEmail").val();
+		console.log(email);
+	}
+	
+	
+	if(emailFormat.test(email)==false)
+	{
+		
+		alert("邮箱格式不正确");
+		return
+	}
+	
+	
+}
+
+function doSignIn()
+{
+
 }
 
 
-function toSingUp
+function toSingUp()
 {
 
 
 }
 
 //evnet
+//$("#buttonSignIn").on('click', verifyEmail());
+
+//$("#buttonSignUp").on('click', verifyEmail());
+
+
+//let
+
+//const
