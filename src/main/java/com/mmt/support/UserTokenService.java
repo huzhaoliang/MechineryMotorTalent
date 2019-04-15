6ba18cfdeae2aa288c3c8326cfc5c056e8d3ff66
huz7 <huz7@10.32.134.12>
2019-04-15 08:56:32 +0800

    add token service

---
+++ src/main/java/com/mmt/support/UserTokenService.java
@@ -0,0 +1,24 @@
+package com.mmt.support;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class UserTokenService 
+{
+	
+	public static String generateToken()
+	{
+		return "";
+	}
+	
+	public static void verifyToken(String _token)
+	{
+		
+	}
+	
+}
