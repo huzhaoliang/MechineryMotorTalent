eebf595dd1b6160eb9579622a4b33fb0bc25dec0
huz7 <huz7@10.91.8.248>
2019-04-28 19:52:04 +0800

    update some code

--- src/main/java/com/mmt/restcontroller/UserRestController.java
+++ src/main/java/com/mmt/restcontroller/UserRestController.java
@@ -3,6 +3,7 @@ package com.mmt.restcontroller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -127,7 +128,7 @@ public class UserRestController
 	//to verify if user is signed in
 	@ResponseBody
 	@RequestMapping(value = "/verifyToken", method = RequestMethod.POST)
-	public String verifyToken(@RequestBody TokenJosnFormat jsonBody)
+	public ResponseEntity<String> verifyToken(@RequestBody TokenJosnFormat jsonBody)
 	{
 		logger.info("######");
 		logger.info(jsonBody.toString());
