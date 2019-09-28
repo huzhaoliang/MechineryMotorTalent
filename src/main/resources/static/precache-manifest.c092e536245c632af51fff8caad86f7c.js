c96eaa44da999e2284e20ba72153d42d726a6809
William Zhou <scy_zhou@163.com>
2019-09-28 11:34:32 +0800

    William removes files

---
+++ src/main/resources/static/precache-manifest.c092e536245c632af51fff8caad86f7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b1a3840e8474966189df140563e19f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43bb10e9c82b9f238680",
+    "url": "/static/css/main.30888a2d.chunk.css"
+  },
+  {
+    "revision": "5ba9f3741d2e2dae708f",
+    "url": "/static/js/2.1a835d73.chunk.js"
+  },
+  {
+    "revision": "43bb10e9c82b9f238680",
+    "url": "/static/js/main.00af727e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
