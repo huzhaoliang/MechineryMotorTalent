034ca977fcdfe6953310f163ac56b70277f258fc
HuZhaoLiang <hp393747035@126.com>
2019-10-14 19:17:26 +0800

    remove files

---
+++ src/main/resources/static/precache-manifest.ac37f1e198cd454cbdb717c0867ea0cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c8928e0adf8d621be464f8904972dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df006600811b832a8a09",
+    "url": "/static/css/main.7eb3209d.chunk.css"
+  },
+  {
+    "revision": "9b6f71b0c940626b1075",
+    "url": "/static/js/2.b83a621b.chunk.js"
+  },
+  {
+    "revision": "df006600811b832a8a09",
+    "url": "/static/js/main.7dff3aec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
